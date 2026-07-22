cfa8c3f850230a43b22eee3d2dba4e68a7bdf700
David Janice <djanice1980@gmail.com>
2026-07-21 22:44:24 -0500

    Bump version to 1.20.0

--- src/app/main_window.cpp
+++ src/app/main_window.cpp
@@ -3827,7 +3827,7 @@ void MainWindow::showAbout() {
     dlg.setWindowTitle("About pcbview");
 
     auto* text = new QLabel(
-        "<h3>pcbview 1.19.1</h3>"
+        "<h3>pcbview 1.20.0</h3>"
         "<p>Standalone 3D PCB viewer. Renders what the fab will build.</p>"
         "<p>Copyright © 2026 pcbview contributors.<br>"
         "pcbview is free software under the <b>GNU General Public License "
